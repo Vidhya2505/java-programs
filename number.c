c3afc8b8da1722c6f43bd405bcdefeb6b1afbda9
vidhyaf1 <154443184+Vidhya2505@users.noreply.github.com>
2024-08-12 16:13:02 +0530

    Add files via upload

---
+++ number.c
@@ -0,0 +1,21 @@
+package programs;
+
+public class sum_of_first_10_natural_numbers {
+
+public static void main(String[] args)
+{
+int i;
+int n;
+n=10;
+int sum=0;
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+// TODO Auto-generated method stub
+
+}
+
+}
+
