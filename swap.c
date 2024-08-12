59a106c220544fc2aa02c0bc19420c2d5291d819
vidhyaf1 <154443184+Vidhya2505@users.noreply.github.com>
2024-08-12 16:02:11 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,20 @@
+package programs;
+
+public class swapping_without_3rdvariable {
+
+public static void main(String[] args)
+{
+int a,b;
+a=1;
+b=2;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a);
+System.out.println(b);
+// TODO Auto-generated method stub
+
+}
+
+}
+
