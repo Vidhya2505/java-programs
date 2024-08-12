097598736dd88b2cd0ffba5c3847ccea07368ffd
vidhyaf1 <154443184+Vidhya2505@users.noreply.github.com>
2024-08-12 16:07:52 +0530

    Add files via upload

---
+++ swapping.c
@@ -0,0 +1,21 @@
+package programs;
+
+public class swapping_with_3rdvariable {
+
+public static void main(String[] args)
+{
+int temp;
+int a=10;
+int b=40;
+temp=a;
+a=b;
+b=temp;
+System.out.println(a);
+System.out.println(b);
+
+// TODO Auto-generated method stub
+
+}
+
+}
+
